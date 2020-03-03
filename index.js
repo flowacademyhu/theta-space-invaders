3d44ce2d933544c13c8e77f99a3b3bb661674ff2
fehercs <feher.csongor@yahoo.com>
2020-03-03 18:37:22 +0100

    created main and sleep function in index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const board = require(`./lib/board/board`);
+const canon = require(`./lib/units/canon`);
+
+const BOARDHEIGHT = 15;
+const BOARDWIDTH = 20;
+const REFRESHRATE = 1000; // in ms
+const MAXLIFE = 2;
+const PLAYERLIMIT = { left: 3, right: 11 };
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+async function main () {
+  const map = board.generateBoard(BOARDHEIGHT, BOARDWIDTH);
+  canon.initPlayer(PLAYERLIMIT.left, MAXLIFE, PLAYERLIMIT.left, PLAYERLIMIT.right, map);
+  canon.playerControl(map);
+  while (true) {
+    console.log(map);
+    await sleep(REFRESHRATE);
+    console.clear();
+  }
+}
+
+main();
