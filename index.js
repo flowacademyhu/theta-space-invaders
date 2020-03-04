af580630365e2bc023a46d71b70cff844cf9316e
Balla Zsolt <ballatanar@gmail.com>
2020-03-04 16:34:38 +0100

    bunkers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const board = require(`./lib/board/board`);
-const canon = require(`./lib/units/canon`);
-
+const board = require('./lib/board/board');
+const canon = require('./lib/units/canon');
+const { initBunker1, initBunker2, initBunker3 } = require('./lib/units/bunker');
 const BOARDHEIGHT = 15;
 const BOARDWIDTH = 20;
 const REFRESHRATE = 1000; // in ms
@@ -14,6 +14,9 @@ const sleep = (ms) => {
 async function main () {
   const map = board.generateBoard(BOARDHEIGHT, BOARDWIDTH);
   canon.initPlayer(PLAYERLIMIT.left, MAXLIFE, PLAYERLIMIT.left, PLAYERLIMIT.right, map);
+  initBunker1(map);
+  initBunker2(map);
+  initBunker3(map);
   canon.playerControl(map);
   while (true) {
     board.printBoard(map);
