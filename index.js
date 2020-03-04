75c47e444803a9139c5d791183a3bbc1667ac222
fehercs <feher.csongor@yahoo.com>
2020-03-04 14:49:23 +0100

    moved global constants to constants.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const board = require(`./lib/board/board`);
-const canon = require(`./lib/units/canon`);
-
-const BOARDHEIGHT = 15;
-const BOARDWIDTH = 20;
-const REFRESHRATE = 1000; // in ms
-const MAXLIFE = 2;
-const PLAYERLIMIT = { left: 3, right: 16 };
+const board = require('./lib/board/board');
+const canon = require('./lib/units/canon');
+const { REFRESHRATE } = require('./lib/constants');
 
 const sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
 
 async function main () {
-  const map = board.generateBoard(BOARDHEIGHT, BOARDWIDTH);
-  canon.initPlayer(PLAYERLIMIT.left, MAXLIFE, PLAYERLIMIT.left, PLAYERLIMIT.right, map);
+  const map = board.generateBoard();
+  canon.initPlayer(map);
   canon.playerControl(map);
   while (true) {
     board.printBoard(map);
