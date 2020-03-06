972a569d08df97d45ffa2bd15ed296b8d42c38eb
Szanka <szankabal@gmail.com>
2020-03-06 14:22:01 +0100

    WTF

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { initPlayer, playerControl } = require('./lib/units/canon');
 const { initBunker1, initBunker2, initBunker3 } = require('./lib/units/bunker');
 const { REFRESHRATE } = require('./lib/constants');
 const { mothershipInit } = require('./lib/units/mothership');
-const { genUfo, startUfo, moveUfo } = require('./lib/units/ufo');
+const { genUfo, startUfo, moveUfo, ufoShoot } = require('./lib/units/ufo');
 
 const sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -20,6 +20,7 @@ async function main() {
   playerControl(map);
   while (true) {
     moveUfo(ufoArr, map);
+    ufoShoot(ufoArr, map);
     printBoard(map);
     mothershipInit(map);
     await sleep(REFRESHRATE);
